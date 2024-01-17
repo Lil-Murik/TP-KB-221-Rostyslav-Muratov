6e8d40c3fe62ab849e749cee50cb0e4864f53a61
unknown <--rostikmuratov646@gmail.com>
2024-01-18 01:49:29 +0200

    add report file

---
+++ lab_03/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, phone, specialty, group):
+        self.name = name
+        self.phone = phone
+        self.specialty = specialty
+        self.group = group
